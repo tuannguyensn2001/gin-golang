e8299e238098c0730f766f33b6e0027c546bb17b
nhokvip Tuan <devpro2001@gmail.com>
2021-06-27 00:51:52 +0700

    test gin

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("hello world")
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", func(context *gin.Context) {
+		user := User{
+			Name: "Tuan",
+			Age:  20,
+		}
+		context.JSON(http.StatusOK, gin.H{
+			"message": "done",
+			"user":    &user,
+		})
+	})
+
+	r.Run(":8080")
+
 }
